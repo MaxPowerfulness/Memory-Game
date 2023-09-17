4c1aca28fb459ebd03281a4a7106cef39970c91c
Sean Gombart <skgombart@gmail.com>
2023-09-17 15:50:51 -0700

    Logic for displaying cards

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,19 +1,24 @@
-function CardCont({ characters, difficulty }) {
-  let cardDeck = [];
-  let i = 0;
-  while (i < difficulty.number) {
-    cardDeck.push(<Card name={characters[i].name} picture={characters[i].image} />);
-    i++;
-  }
-  return <main>{cardDeck}</main>;
+import { v4 } from 'uuid';
+
+function CardCont({ characters }) {
+  console.log('card chars', characters);
+  let cardDeck = characters.map((character) => <Card key={v4()} name={character.name} picture={character.image} />);
+  return (
+    <main>
+      <ul>{cardDeck}</ul>
+      <p>Test</p>
+    </main>
+  );
 }
 
 function Card({ name, picture }) {
   return (
-    <div>
-      <h2>{name}</h2>
-      <img src={picture} alt="Character" />
-    </div>
+    <li>
+      <div>
+        <h2>{name}</h2>
+        <img src={picture} alt="Character" />
+      </div>
+    </li>
   );
 }
 export default CardCont;
