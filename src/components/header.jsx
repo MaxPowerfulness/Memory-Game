d8c561e4ffc8e6ef9052236f09127ad2574ec213
Sean Gombart <skgombart@gmail.com>
2023-09-17 15:51:19 -0700

    Added difficulty settings button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,11 @@
 import Scoreboard from './scoreboard';
 
-function Header({ bestScore, currentScore }) {
+function Header({ bestScore, currentScore, onDifficulty, difficulty }) {
   return (
     <header>
       <h1>Memory Game</h1>
       <Scoreboard bestScore={bestScore} currentScore={currentScore} />
+      <button onClick={onDifficulty}>{difficulty.difficulty}</button>
     </header>
   );
 }
